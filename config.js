4ae46599b0b9c877f81ce3df155d165804df81c9
gamingkenzo <111523835+gamingkenzo@users.noreply.github.com>
2022-08-26 16:03:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 //SOME ENCRYPTED FILES
-//THANKS FOR CHOOSING VIHANGAMD
+//THANKS FOR CHOOSING KENZOMOD
 
 const fs = require('fs')
 const chalk = require('chalk')
@@ -17,25 +17,25 @@ global.APIKeys = {
 }
 
 //settings
-global.welcomeandgoodbye = 'true' //welcome එක off කරන්න ඕනෙ නම් false දාන්න.
+global.welcomeandgoodbye = 'false' //welcome එක off කරන්න ඕනෙ නම් false දාන්න.
 global.callblock = 'false' //call block on කරන්න ඕනෙ නම් false කියල තියෙන එක මකල true කියල දාන්න.
 
 //Owner React
-global.OWNER_REACT = '🧑🏻‍💻' //මෙතනට emoji එකක දාන්න.
-global.REACT_NUMBER = '9476342733' //මෙතන number එක.
+global.OWNER_REACT = '' //මෙතනට emoji එකක දාන්න.
+global.REACT_NUMBER = '9463867158' //මෙතන number එක.
 
 // Other
-global.owner = ['9476342733',''] //මෙතන number එක.
-global.premium = ['9476342733','9476342733'] //මෙතන number එක.
-global.botname = 'VIHANGA BOT INC' //මෙතන නම.
-global.ownername = 'Vihanga MD' //මෙතන නම.
-global.ownername = 'ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ' //මෙතන නම.
-global.group1 = 'https://chat.whatsapp.com/GJ9bAPxYpktAnqbXVsnRuf' //මෙතන grouplink එක.
-global.group2 = 'https://chat.whatsapp.com/GJ9bAPxYpktAnqbXVsnRuf' //මෙතන grouplink එක.
-global.packname = 'VIHANGA BOT INC' //මෙතන නම.
-global.cap = '*By Vihanga MD*' //මෙතන නම.
-global.img = 'https://telegra.ph/file/88d6fdeff08bbc4945901.jpg' //මෙතන බොට්ගෙ photo එකට ඕන derect link එකක්.
-global.ytchannel = 'https://youtu.be/Z9rIR0Vopb8' //මෙතන කැමති ලින්ක් එකක්.
+global.owner = ['94763867158',''] //මෙතන number එක.
+global.premium = ['94763867158','94763867158'] //මෙතන number එක.
+global.botname = '❰❰⦁ 𝛫𝛯𝘕𝘡𝘖 ⦁❱❱' //මෙතන නම.
+global.ownername = '❰❰⦁ 𝛫𝛯𝘕𝘡𝘖 ⦁❱❱' //මෙතන නම.
+global.ownername = '❰❰⦁ 𝛫𝛯𝘕𝘡𝘖 ⦁❱❱' //මෙතන නම.
+global.group1 = 'https://chat.whatsapp.com/H047x1gxkO9F6z3UhoiMef' //මෙතන grouplink එක.
+global.group2 = 'https://chat.whatsapp.com/H047x1gxkO9F6z3UhoiMef' //මෙතන grouplink එක.
+global.packname = '❰❰⦁ 𝛫𝛯𝘕𝘡𝘖 ⦁❱❱' //මෙතන නම.
+global.cap = '*BY ❰❰⦁ 𝛫𝛯𝘕𝘡𝘖 ⦁❱❱*' //මෙතන නම.
+global.img = 'https://i.ibb.co/VTxVmF6/20220821-153441.jpg .jpg' //මෙතන බොට්ගෙ photo එකට ඕන derect link එකක්.
+global.ytchannel = 'https://youtube.com/channel/UCxqpAzaAecDfYOPywxUJoyw' //මෙතන කැමති ලින්ක් එකක්.
 global.monayawal = '100000' //මෙතන මාරු කරන්න එපා.
 global.darahawal = '100' //මෙතන මාරු කරන්න එපා.
 global.besiAwal = '15' //මෙතන මාරු කරන්න එපා.
@@ -56,7 +56,7 @@ global.f3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.docu
 global.f4 = 'application/zip' //මෙතන මාරු කරන්න එපා.
 global.f5 = 'application/pdf' //මෙතන මාරු කරන්න එපා.
 global.f6 = 'application/vnd.android.package-archive' //මෙතන මාරු කරන්න එපා.
-global.author = 'VIHANGA BOT INC' //මෙතන නම.
+global.author = '❰❰⦁ 𝛫𝛯𝘕𝘡𝘖 ⦁❱❱ BOT INC' //මෙතන නම.
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=' //මෙතන මාරු කරන්න එපා.
 global.sessionName = 'session' //මෙතන මාරු කරන්න එපා.
 global.prefa = ['','!','.','🐦','🐤','🗿'] //මෙතන මාරු කරන්න එපා.
